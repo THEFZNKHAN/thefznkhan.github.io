ee03e36a49ee72149b1be2b5e87f21dcce8343c4
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2023-10-01 02:49:01 +0530

    Making Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from "./pages/about/About";
 import Portfolio from "./pages/portfolio/Portfolio";
 import Contact from "./pages/contact/Contact";
 import "./app.css";
+import "./index.css";
 
 function App() {
     return (
